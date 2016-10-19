ae6ce07da0b7445f5973280f7b96d88edc5bbee3
RobsyRocket <robert.roemer@live.de>
2016-10-19 19:02:54 +0200

    FP2800a implementation to work with multiple connected ICs via one single controller instance. The available outputs get joined together and the controller takes care to map the selected output to the correct output on the individual ICs.

---
+++ Code/Arduino/libraries/FlipTheDot_FP2800a/FlipTheDot_FP2800aMulti.h
@@ -0,0 +1,194 @@
+/*
+ * FlipTheDot_FP2800aMulti Class
+ * 
+ * Works like the normal FP2800a class but utilize multiple ICs and maps the selected output to the correct IC.
+ * The several control pins (A0-B1 and Data) of all ICs are wired to the same microcontroller pins, except the enable pins which are separated and get individually controlled.
+ *
+ * @author Robert Römer <robert.roemer@live.de>
+ */
+
+
+
+#ifndef FlipTheDot_FP2800aMulti_h
+#define FlipTheDot_FP2800aMulti_h
+
+#include "Arduino.h"
+#include "FlipTheDot_FP2800a.h"
+
+
+
+class FlipTheDot_FP2800aMulti : public FlipTheDot_FP2800a
+{
+    public:
+        // nearly identical parameters like FP2800a but with enable pin list and its length informations
+        FlipTheDot_FP2800aMulti(unsigned int pinEnableList[], unsigned int pinEnableListLength, unsigned int pinData, unsigned int pinA0, unsigned int pinA1, unsigned int pinA2, unsigned int pinB0, unsigned int pinB1, unsigned int pulseLengthMillis);
+        bool setOutput(unsigned int no);
+        unsigned int getOutput();
+        unsigned int getOutputMax();
+        void FlipTheDot_FP2800aMulti::_initPins();
+    
+    protected:
+        byte _selectedEnableNo = 0;
+        unsigned int *_pinEnableList;
+        unsigned int _pinEnableListLength = 0;
+        bool _hasDuplicatePins();
+};
+
+
+FlipTheDot_FP2800aMulti::FlipTheDot_FP2800aMulti(unsigned int pinEnableList[], unsigned int pinEnableListLength, unsigned int pinData, unsigned int pinA0, unsigned int pinA1, unsigned int pinA2, unsigned int pinB0, unsigned int pinB1, unsigned int pulseLengthMillis = 100) : FlipTheDot_FP2800a(pinEnableList[0], pinData, pinA0, pinA1, pinA2, pinB0, pinB1, pulseLengthMillis)
+{
+    // the value for key 0 of pinEnableList gets immediatly forwarded to the parent constructor
+
+    // storing the provided list and length of enable pins
+    _pinEnableList = pinEnableList;
+    _pinEnableListLength = pinEnableListLength;
+    
+    // following instruction are already called in the parent constructor but need to be called again to count in the _pinEnableList values
+    {
+        // check for duplicates
+        if ( _hasDuplicatePins() )
+        {
+          // some of the pins are equal => not allowed
+          #ifdef FlipTheDot_FP2800aMulti_DEBUG_SERIAL
+          //FlipTheDot_FP2800aMulti_DEBUG_SERIAL.println("Pin configuration for FlipTheDot_FP2800aMulti is not valid: duplicate pin detected");
+          #endif
+          while(1);
+        }
+
+        // call initPins again, like in the parent constructor, to check the whole list of enable pins
+        _initPins();
+    }
+}
+
+
+void FlipTheDot_FP2800aMulti::_initPins()
+{
+    // call parent _initPins method 
+    FlipTheDot_FP2800a::_initPins();
+    
+    // loop thru all enable pins, except the first one which was initialized in the parent _initPins call
+    for ( byte i=0; i < _pinEnableListLength; i++)
+    {
+        pinMode(_pinEnableList[i], OUTPUT);
+        digitalWrite(_pinEnableList[i], LOW);
+    }
+}
+
+/**
+ * Check if one or more pins are identical and referencing the same hardware IO.
+ * The list of all enable pins will be compared to the other functional pin defintions.
+ */
+bool FlipTheDot_FP2800aMulti::_hasDuplicatePins()
+{
+    // loop thru the list of all enable pins
+    for ( byte i=0; i < _pinEnableListLength; i++)
+    {
+        // temporary change selected enable pin no
+        _pinEnable = _pinEnableList[i];
+
+        // run the regular duplicate check
+        if ( FlipTheDot_FP2800a::_hasDuplicatePins() )
+        {
+            // set the enable pin back to the one which was set before this method call
+            _pinEnable = _pinEnableList[_selectedEnableNo];
+
+            // stop because duplicate detected
+            return true;
+        }
+    }
+    // set the enable pin back to the one which was set before this method call
+    _pinEnable = _pinEnableList[_selectedEnableNo];
+        
+    // no duplicate detected
+    return false;
+}
+
+
+/**
+ * Map the output no to the corresponding enable pin and its outputs.
+ */
+bool FlipTheDot_FP2800aMulti::setOutput(unsigned int no)
+{
+    // calculate which enable pin (chip / display) should be selected
+    byte enable_no = no / _maxOutputsOnChip;
+
+    // recalculation of the output in relation to the selected enable pin
+    no = no % _maxOutputsOnChip;
+    
+    // special handling if previous calculation returning zero
+    if ( no == 0 )
+    {
+        enable_no -= enable_no > 0 ? 1 : 0;
+        no = _maxOutputsOnChip;
+    }
+    
+    // check if the enable_no is out of range
+    if ( enable_no >= _pinEnableListLength )
+    {
+        #ifdef FlipTheDot_FP2800aMulti_DEBUG_SERIAL
+        FlipTheDot_FP2800aMulti_DEBUG_SERIAL.print("FlipTheDot_FP2800aMulti selected enable pin ");
+        FlipTheDot_FP2800aMulti_DEBUG_SERIAL.print(no);
+        FlipTheDot_FP2800aMulti_DEBUG_SERIAL.print(" is out of range 0 to ");
+        FlipTheDot_FP2800aMulti_DEBUG_SERIAL.println(_pinEnableListLength-1);
+        #endif
+        return false;
+    }
+    
+    // check if the desired output is not already selected (ignoring the previously selected enable pin)
+    if ( _selectedOutput != no )
+    {
+        // try to set the recalculated output no like defined in the parent implementation
+        if ( !FlipTheDot_FP2800a::setOutput(no) )
+        {
+            // output could not be set, the enable pin was maybe active
+            return false;
+        }
+    }
+
+    // change enable pin
+    _selectedEnableNo = enable_no;
+    _pinEnable = _pinEnableList[_selectedEnableNo];
+    
+    #ifdef FlipTheDot_FP2800aMulti_DEBUG_SERIAL
+    FlipTheDot_FP2800aMulti_DEBUG_SERIAL.print("FlipTheDot_FP2800aMulti_DEBUG_SERIAL output ");
+    FlipTheDot_FP2800aMulti_DEBUG_SERIAL.print(enable_no*_maxOutputsOnChip+no);
+    FlipTheDot_FP2800aMulti_DEBUG_SERIAL.print(" selected (mapped to ");
+    FlipTheDot_FP2800aMulti_DEBUG_SERIAL.print(no);
+    FlipTheDot_FP2800aMulti_DEBUG_SERIAL.print(" and enable pin ");
+    FlipTheDot_FP2800aMulti_DEBUG_SERIAL.print(enable_no+1);
+    FlipTheDot_FP2800aMulti_DEBUG_SERIAL.println(")");
+    #endif
+    return true;
+}
+
+
+/**
+ * get selected output
+ */
+unsigned int FlipTheDot_FP2800aMulti::getOutput()
+{
+    return _selectedOutput + (_selectedEnableNo * _maxOutputsOnChip);
+}
+
+
+/**
+ * get maximum output number
+ */
+unsigned int FlipTheDot_FP2800aMulti::getOutputMax()
+{
+    #ifdef FlipTheDot_FP2800aMulti_DEBUG_SERIAL
+    FlipTheDot_FP2800aMulti_DEBUG_SERIAL.print("FlipTheDot_FP2800aMulti_DEBUG_SERIAL max outputs: ");
+    FlipTheDot_FP2800aMulti_DEBUG_SERIAL.print(_maxOutputsOnChip * _pinEnableListLength);
+    FlipTheDot_FP2800aMulti_DEBUG_SERIAL.print(" (");
+    FlipTheDot_FP2800aMulti_DEBUG_SERIAL.print(_maxOutputsOnChip);
+    FlipTheDot_FP2800aMulti_DEBUG_SERIAL.print(" * ");
+    FlipTheDot_FP2800aMulti_DEBUG_SERIAL.print(_pinEnableListLength);
+    FlipTheDot_FP2800aMulti_DEBUG_SERIAL.println(")");
+    #endif
+    return _maxOutputsOnChip * _pinEnableListLength;
+}
+
+
+
+
+#endif // FlipTheDot_FP2800aMulti_h
---
+++ Code/Arduino/libraries/FlipTheDot_FP2800a/examples/Multi/Multi.ino
@@ -0,0 +1,102 @@
+/*
+  Multi
+  Iterate thru the output numbers from 1 to 28 of multiple ICs and both data pin states, repeatedly.
+
+  More details are available in the example "Basic" and the definition of class FlipTheDot_FP2800a.
+  
+  This example code is in the public domain.
+
+  modified 19 October 2016
+  by Robert Römer
+ */
+
+
+// uncomment these lines to get some debug informations via the defined Serial connection
+/*
+*/
+#ifndef FlipTheDot_FP2800aMulti_DEBUG_SERIAL
+#define FlipTheDot_FP2800aMulti_DEBUG_SERIAL Serial
+#endif
+
+// include the library
+#include "FlipTheDot_FP2800aMulti.h"
+
+
+// defining the Arduino pins which control the FP2800a ICs
+const int fp2800a_pin_DATA      = A0;
+
+const int fp2800a_pin_ENABLE_1  = A1;
+const int fp2800a_pin_ENABLE_2  = A2;
+const int fp2800a_pin_ENABLE_3  = A3;
+const int fp2800a_pin_ENABLE_4  = A4;
+const int fp2800a_pin_ENABLE_5  = A5;
+
+const int fp2800a_pin_A0        = 2; // D2
+const int fp2800a_pin_A1        = 3; // D3
+const int fp2800a_pin_B0        = 4; // D4
+const int fp2800a_pin_B1        = 5; // D5
+const int fp2800a_pin_B2        = 6; // D6
+
+const int fp2800a_pulse_length  = 100; // microseconds
+
+
+// prepare the list of enable pins
+int fp2800a_pin_ENABLE_list[] = {
+  fp2800a_pin_ENABLE_1,
+  fp2800a_pin_ENABLE_2,
+  fp2800a_pin_ENABLE_3,
+  fp2800a_pin_ENABLE_4,
+  fp2800a_pin_ENABLE_5
+};
+
+
+// setup the FP2800aMulti controller object
+FlipTheDot_FP2800aMulti controller(
+                            fp2800a_pin_ENABLE_list,
+                            sizeof(fp2800a_pin_ENABLE_list) / sizeof(int), // list length/size (calculated or static value possible)
+                            fp2800a_pin_DATA,
+                            fp2800a_pin_A0,
+                            fp2800a_pin_A1,
+                            fp2800a_pin_B0,
+                            fp2800a_pin_B1,
+                            fp2800a_pin_B2,
+                            fp2800a_pulse_length
+);
+
+// helper variables
+int outputNo = 0;
+boolean dataStatus = false;
+
+
+
+void setup() {
+  // initialize debug Serial connection if defined
+  #ifdef FlipTheDot_FP2800aMulti_DEBUG_SERIAL
+  FlipTheDot_FP2800aMulti_DEBUG_SERIAL.begin(9600);
+  #endif
+
+  delay(1000);
+  controller.setData( !dataStatus );
+}
+
+
+void loop() {
+  if ( outputNo < 0 || outputNo >= controller.getOutputMax() )
+  {
+    // reset output number and invert the data flag
+    outputNo = 0;
+    dataStatus = !dataStatus;
+    controller.setData( dataStatus );
+  }
+
+  // increase output number
+  outputNo++;
+
+  // set new output number
+  controller.setOutput( outputNo );
+
+  // pulse the "enable" pin for the defined time to high and back to low
+  controller.pulse();
+
+  delay(500);
+}
